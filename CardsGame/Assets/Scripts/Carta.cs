5f5b41319bc8f32d24480e5b1ca8178ee20ceef2
Enrique Solís <jose11@windowslive.com>
2022-03-16 01:27:04 -0400

    Funcionamiento basico

---
+++ CardsGame/Assets/Scripts/Carta.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Carta : MonoBehaviour
+{
+    [HideInInspector]
+    public int tipo;
+
+    [HideInInspector]
+    public bool estaGirada, estaLista;
+
+    GameManager gameManager;
+    Image imagen;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        imagen = GetComponent<Image>();
+        Ocultar();
+    }
+
+    public void AsignarSprite(Sprite sprite, int tipo)
+    {
+        this.tipo = tipo;
+        GetComponent<Image>().sprite = sprite;
+    }
+
+    public void LlamarGiro()
+    {
+        StartCoroutine(Girar());
+    }
+
+    private IEnumerator Girar()
+    {
+        if (!estaLista && !estaGirada && gameManager.numeroDeCartasGiradas < gameManager.limiteCartasGiradas)
+        {
+            Mostrar();
+            yield return new WaitForSeconds(gameManager.tiempoDeGiro);
+            
+            if(!estaLista)
+                Ocultar();
+        }
+    }
+
+    private void Ocultar()
+    {
+        estaGirada = false;
+        imagen.color = Color.black;
+    }
+
+    private void Mostrar()
+    {
+        estaGirada = true;
+        imagen.color = Color.white;
+    }
+
+    public void Descartar()
+    {
+        estaLista = true;
+        imagen.color = Color.red;
+    }
+}
---
+++ CardsGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    public int limiteCartasGiradas, maximoDeCartasDelMismoTipo;
+
+    public float tiempoDeGiro;
+
+    public GameObject cartaPrefab, contenidoDeCartas;
+
+    [HideInInspector]
+    public int numeroDeCartasGiradas => cartasGiradas.Length;
+
+    [HideInInspector]
+    public List<Carta> cartasEnTablero;
+    
+    private Carta[] cartasGiradas;
+
+    private bool estaComparando;
+
+    private List<Sprite> tipos;
+
+    private void Start()
+    {
+        tipos = sprites.ToList();
+        RepartirCartas();
+    }
+
+    void RepartirCartas()
+    {
+        int numeroDeCartas = Random.Range(4, sprites.Length / 2);
+
+        for (int c = 0; c < numeroDeCartas; c++)
+        {
+            int tipo = Random.Range(0, tipos.Count);
+            InstanciarCartas(tipo);
+        }
+        
+        List<Carta> cartas = new List<Carta>(cartasEnTablero);
+
+        for (int c = 0; c < numeroDeCartas * limiteCartasGiradas; c++)
+        {
+            int index = Random.Range(0, cartas.Count);
+            cartas[index].transform.SetParent(contenidoDeCartas.transform);
+            cartas.Remove(cartas[index]);
+        }
+    }
+
+    private void InstanciarCartas(int tipo)
+    {
+        for (int c = 0; c < limiteCartasGiradas; c++)
+        {
+            Carta carta = Instantiate(cartaPrefab).GetComponent<Carta>();
+            cartasEnTablero.Add(carta);
+            Debug.Log($"Tipo: " + tipo + " Tipos: " + tipos.Count);
+            carta.AsignarSprite(tipos[tipo], tipo);
+        }
+
+        DescartarTipos();
+    }
+
+    private void Update()
+    {
+        cartasGiradas = System.Array.FindAll(cartasEnTablero.ToArray(), carta => carta.estaGirada && !carta.estaLista);
+
+        if (numeroDeCartasGiradas >= limiteCartasGiradas && !estaComparando)
+            CompararTipos();     
+    }
+
+    void CompararTipos()
+    {
+        estaComparando = true;
+        Carta primeraCarta = cartasGiradas[0];
+
+        for(int c=1; c <cartasGiradas.Length; c++)
+        {
+            Carta carta = cartasGiradas[c];
+
+            if (carta.tipo == primeraCarta.tipo)
+            {
+                carta.Descartar();
+
+                if (!primeraCarta.estaLista)
+                    primeraCarta.Descartar();
+            }
+        }
+
+        estaComparando = false;
+    }
+
+    void DescartarTipos()
+    {
+        foreach (Sprite sprite in tipos.ToArray())
+        {
+            int tipo = System.Array.FindIndex(tipos.ToArray(), item => item.name == sprite.name);
+            int cartasDelMismoTipo = System.Array.FindAll(cartasEnTablero.ToArray(), carta => carta.tipo == tipo).Length;
+            if (cartasDelMismoTipo >= maximoDeCartasDelMismoTipo)
+            {
+                tipos.Remove(tipos[tipo]);
+            }
+        }
+
+    }
+
+    public void ReiniciarJuego()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+}
